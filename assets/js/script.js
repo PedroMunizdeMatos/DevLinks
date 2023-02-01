e9dbc4c03b7395307d70d9e9786ee290e23c324a
Pedro Muniz <phmunizdematos@gmail.com>
2023-02-01 16:34:46 -0300

    animation switch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
 function toggleMode() {
   const html = document.documentElement
 
-  html.classList.toggle("light")
+  html.classList.toggle('light')
 
-  const img = document.querySelector("#profile img")
+  const img = document.querySelector('#profile img')
 
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "../assets/img/pedroBgLight.png")
+  if (html.classList.contains('light')) {
+    img.setAttribute('src', './assets/img/pedroBgLight.png')
     img.setAttribute(
-      "alt",
-      "Pedro sorrindo com fundo degradê de roxo para azul claro"
+      'alt',
+      'Pedro sorrindo com fundo degradê de roxo para azul claro'
     )
   } else {
-    img.setAttribute("src", "../assets/img/pedroBgDark.png")
-    img.setAttribute("alt", "Pedro sorrindo com fundo laranja")
+    img.setAttribute('src', './assets/img/pedroBgDark.png')
+    img.setAttribute('alt', 'Pedro sorrindo com fundo laranja')
   }
 }
