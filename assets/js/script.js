0cf0f8f8feca33c85e38b3df4edf065b302d0bac
Pedro Muniz <phmunizdematos@gmail.com>
2023-02-01 18:07:15 -0300

    path js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,13 @@ function toggleMode() {
   const img = document.querySelector('#profile img')
 
   if (html.classList.contains('light')) {
-    img.setAttribute('src', '../assets/img/pedroBgLight.png')
+    img.setAttribute('src', './assets/img/pedroBgLight.png')
     img.setAttribute(
       'alt',
       'Pedro sorrindo com fundo degradê de roxo para azul claro'
     )
   } else {
-    img.setAttribute('src', '../assets/img/pedroBgDark.png')
+    img.setAttribute('src', './assets/img/pedroBgDark.png')
     img.setAttribute('alt', 'Pedro sorrindo com fundo laranja')
   }
 }
